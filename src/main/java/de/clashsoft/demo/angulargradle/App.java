2ef4c8b389bbe4be56eea202c76c50abad135d26
Adrian Kunz <clashsoft@hotmail.com>
2020-01-30 01:33:21 +0100

    Add Spark Service and static files config in App main

--- src/main/java/de/clashsoft/demo/angulargradle/App.java
+++ src/main/java/de/clashsoft/demo/angulargradle/App.java
@@ -1,8 +1,16 @@
 package de.clashsoft.demo.angulargradle;
 
+import spark.Service;
+
 public class App
 {
 	public static void main(String[] args)
 	{
+		final Service service = Service.ignite();
+		service.port(1234);
+
+		service.staticFileLocation("/");
+
+		service.init();
 	}
 }
