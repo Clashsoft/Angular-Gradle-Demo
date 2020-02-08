5cfd6c78f33947cbba588aad3399f82f8ead57f6
Adrian Kunz <clashsoft@hotmail.com>
2020-02-08 21:09:07 +0100

    Use default port (4567) and allow CORS in Spark server

--- src/main/java/de/clashsoft/demo/angulargradle/App.java
+++ src/main/java/de/clashsoft/demo/angulargradle/App.java
@@ -7,11 +7,14 @@ public class App
 	public static void main(String[] args)
 	{
 		final Service service = Service.ignite();
-		service.port(1234);
 
 		service.staticFileLocation("/");
 
 		service.get("/hello", (req, res) -> "Hello from Spark Server");
+
+		service.before((request, response) -> {
+			response.header("Access-Control-Allow-Origin", "http://localhost:4200");
+		});
 
 		service.init();
 	}
