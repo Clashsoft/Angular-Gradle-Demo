91b425eb6ef1e1c3599e25fb0f6c2b5a623e4e32
Adrian Kunz <clashsoft@hotmail.com>
2020-02-08 21:08:45 +0100

    Call API GET method in app component

--- angular/angular-gradle-demo/src/app/app.component.ts
+++ angular/angular-gradle-demo/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {HttpClient} from '@angular/common/http';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-gradle-demo';
+  sparkMessage = 'fetching spark message...';
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.http.get(environment.apiURL + '/hello', {responseType: 'text'})
+      .subscribe(message => this.sparkMessage = message);
+  }
 }
